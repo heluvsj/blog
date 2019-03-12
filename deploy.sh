5acfd4f012d01cbf3732ee0e609e5d30d531c164
heluvsj <godluvsj@gmail.com>
2019-03-12 16:44:59 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ bundle exec jekyll build
 git add .
 git commit -m "Site $(date +%Y%m%d.%H:%M:%S)"
 git push origin site
-git subtree push --prefix _site/ origin gh-pages
+git push origin `git subtree split --prefix _site/ site`:gh-pages --force
 git checkout master
 
 ALGOLIA_API_KEY='024ca1b216683cd62586b133fd8c8a29' bundle exec jekyll algolia
